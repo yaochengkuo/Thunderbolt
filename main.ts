452e575a9cfe618e70fb647d6a1c7bc5d8aa183a
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-11-10 15:40:30 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let y = 0
+let x = 0
+let dx = 1
+basic.forever(function () {
+    basic.clearScreen()
+    y = 0
+    if (x >= 4) {
+        dx = -1
+    } else if (x <= 0) {
+        dx = 1
+    }
+    x += dx
+    for (let index = 0; index < 5; index++) {
+        led.plot(x, y)
+        y += 1
+    }
+    basic.pause(100)
+})
