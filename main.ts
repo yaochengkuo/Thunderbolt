18083ed89f57d2ab8f920da7dc290fcc215d051a
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-11-10 16:47:29 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
+let _2x = 0
+let _1x = 0
 let y = 0
 let x = 0
 let dx = 1
 basic.forever(function () {
     basic.clearScreen()
     y = 0
-    if (x >= 4) {
+    if (x == 4 && (_1x == 4 && _2x == 4)) {
         dx = -1
-    } else if (x <= 0) {
+    } else if (x == 0 && (_1x == 0 && _2x == 0)) {
         dx = 1
     }
     x += dx
+    _1x = dx
+    _2x = dx
     for (let index = 0; index < 5; index++) {
+        led.plotBrightness(_1x, y, 206)
+        led.plotBrightness(_2x, y, 140)
         led.plot(x, y)
         y += 1
     }
